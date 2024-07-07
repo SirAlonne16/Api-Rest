72c2a545f11925e6ef55a644110a0024357bf64d
SirAlonne16 <ben.pobletea@duocuc.cl>
2024-07-07 17:06:58 -0400

    a ver si esto es de tu talla

---
+++ database/migrations/2024_07_07_000000_create_administrador_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdministradorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Administrador', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('rut')->unique();
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Administrador');
+    }
+}
