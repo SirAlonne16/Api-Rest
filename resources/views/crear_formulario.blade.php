2eca584b11cc71d2c70d6125ba0489dfb05bfce1
Svboibg20 <141652861+Svboibg20@users.noreply.github.com>
2024-07-09 04:35:53 -0400

    pipipi

--- resources/views/crear_formulario.blade.php
+++ resources/views/crear_formulario.blade.php
@@ -96,58 +96,12 @@
     <!-- Incluir un script para cargar regiones y comunas -->
     <script>
         $(document).ready(function () {
-            // Datos ficticios de regiones y comunas de Chile
+            // Aquí puedes cargar dinámicamente las regiones y comunas de Chile
             const regionesComunas = {
-                "Región de Arica y Parinacota": [
-                    "Arica", "Camarones", "Putre", "General Lagos"
-                ],
-                "Región de Tarapacá": [
-                    "Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"
-                ],
-                "Región de Antofagasta": [
-                    "Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "María Elena", "Tocopilla"
-                ],
-                "Región de Atacama": [
-                    "Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"
-                ],
-                "Región de Coquimbo": [
-                    "La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"
-                ],
-                "Región de Valparaíso": [
-                    "Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"
-                ],
-                "Región Metropolitana de Santiago": [
-                    "Santiago", "Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"
-                ],
-                "Región de O'Higgins": [
-                    "Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"
-                ],
-                "Región del Maule": [
-                    "Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"
-                ],
-                "Región de Ñuble": [
-                    "Chillán", "Bulnes", "Cobquecura", "Coelemu", "Ninhue", "Portezuelo", "Quirihue", "Ránquil", "Treguaco", "San Carlos", "San Fabián", "San Ignacio", "San Nicolás"
-                ],
-                "Región del Biobío": [
-                    "Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"
-                ],
-                "Región de La Araucanía": [
-                    "Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre Las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol"
-                ],
-                "Región de Los Ríos": [
-                    "Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"
-                ],
-                "Región de Los Lagos": [
-                    "Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"
-                ],
-                "Región de Aysén del General Carlos Ibáñez del Campo": [
-                    "Coyhaique", "Lago Verde", "Aysén", "Cisnes", "Guaitecas", "Cochrane", "O'Higgins", "Tortel", "Chile Chico", "Río Ibáñez"
-                ],
-                "Región de Magallanes y de la Antártica Chilena": [
-                    "Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"
-                ]
+                "Region1": ["Comuna1", "Comuna2"],
+                "Region2": ["Comuna3", "Comuna4"]
+                // Agrega todas las regiones y comunas aquí
             };
-
 
             const regionesSelect = $('#region');
             const comunasSelect = $('#comuna');
@@ -165,44 +119,42 @@
                     comunasSelect.append(new Option(comuna, comuna));
                 });
             });
-        });
 
+            // Generar usuario
+            $('#nombre').on('input', function () {
+                generarUsuario();
+            });
 
-        // Generar usuario
-        $('#nombre').on('input', function () {
-            generarUsuario();
-        });
+            function generarUsuario() {
+                var nombreCompleto = $('#nombre').val().trim();
+                var partes = nombreCompleto.split(' ');
 
-        function generarUsuario() {
-            var nombreCompleto = $('#nombre').val().trim();
-            var partes = nombreCompleto.split(' ');
-
-            if (partes.length >= 2) {
-                var nombre = partes[0];
-                var apellido = partes[1];
-                var primerasLetrasNombre = nombre.substring(0, 3);
-                var primerasLetrasApellido = apellido.substring(0, 3);
-                var usuario = primerasLetrasNombre + primerasLetrasApellido;
-                $('#usuario').text(usuario);
-                $('#usuario_oculto').val(usuario);
-            } else {
-                $('#usuario').text('');
-                $('#usuario_oculto').val('');
+                if (partes.length >= 2) {
+                    var nombre = partes[0];
+                    var apellido = partes[1];
+                    var primerasLetrasNombre = nombre.substring(0, 3);
+                    var primerasLetrasApellido = apellido.substring(0, 3);
+                    var usuario = primerasLetrasNombre + primerasLetrasApellido;
+                    $('#usuario').text(usuario);
+                    $('#usuario_oculto').val(usuario);
+                } else {
+                    $('#usuario').text('');
+                    $('#usuario_oculto').val('');
+                }
             }
-        }
 
-        $('#problemas_medicos').change(function () {
-            // Cuando cambia el valor del select con id problemas_medicos
-            if ($(this).val() === 'Sí') {
+            $('#problemas_medicos').change(function () {
+                // Cuando cambia el valor del select con id problemas_medicos
+                if ($(this).val() === 'Sí') {
 
-                $('#problema_medico').show(); // Mostrar el campo de entrada para problemas médicos
-            } else {
+                    $('#problema_medico').show(); // Mostrar el campo de entrada para problemas médicos
+                } else {
 
-                $('#problema_medico').hide(); // Ocultar el campo de entrada para problemas médicos
-            }
-        }).trigger('change'); // Disparar el evento change para ejecutar la función al cargar la página
+                    $('#problema_medico').hide(); // Ocultar el campo de entrada para problemas médicos
+                }
+            }).trigger('change'); // Disparar el evento change para ejecutar la función al cargar la página
 
-
+        });
 
         function enviarFormulario(event) {
             event.preventDefault(); // Previene el envío del formulario
