8476cd2cf299ed81af11cf2c0e1ec034161054b7
SirAlonne16 <ben.pobletea@duocuc.cl>
2024-07-09 02:52:44 -0400

    Update Formulario.php

--- app/Models/Formulario.php
+++ app/Models/Formulario.php
@@ -6,7 +6,7 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class Formulario extends Eloquent
 {
-    protected $collection = 'clientes';
+    protected $collection = 'formulario';
 
     protected $fillable = [
         'nombre', 'fecha_nacimiento', 'rut', 'email', 'telefono', 'edad', 'direccion', 'region', 'comuna', 'problemas_medicos', 'problema_medico', 'necesita_equipo', 'tiene_experiencia', 'genero', 'usuario'
