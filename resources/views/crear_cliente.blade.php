5a53da51e7078d0dd72804128cab6267c999d4e2
SirAlonne16 <ben.pobletea@duocuc.cl>
2024-07-09 01:25:55 -0400

    Update crear_cliente.blade.php

--- resources/views/crear_cliente.blade.php
+++ resources/views/crear_cliente.blade.php
@@ -2,6 +2,31 @@
 <html>
 <head>
     <title>Crear Cliente</title>
+    <!-- Incluir jQuery desde una CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#nombre').on('input', function() {
+                generarCorreo();
+            });
+
+            function generarCorreo() {
+                var nombreCompleto = $('#nombre').val().trim();
+                var partes = nombreCompleto.split(' ');
+
+                if (partes.length >= 2) {
+                    var nombre = partes[0];
+                    var apellido = partes[1];
+                    var primerasLetrasNombre = nombre.substring(0, 3);
+                    var primerasLetrasApellido = apellido.substring(0, 3);
+                    var correo = primerasLetrasNombre + primerasLetrasApellido + '@gmail.com';
+                    $('#email').val(correo);
+                } else {
+                    $('#email').val('');
+                }
+            }
+        });
+    </script>
 </head>
 <body>
     <h1>Crear Cliente</h1>
@@ -17,7 +42,7 @@
         <input type="text" id="rut" name="rut"><br>
 
         <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
+        <input type="email" id="email" name="email" readonly><br>
 
         <label for="talla">Talla:</label><br>
         <input type="text" id="talla" name="talla"><br>
@@ -25,4 +50,5 @@
         <input type="submit" value="Guardar">
     </form>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </html>
